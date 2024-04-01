6ffd7eb48aa4c4bcaaee8431d3fed571cff3a6f1
Chun-wei-Ho <chunweiho1999@gmail.com>
2024-04-01 23:36:01 +0000

    :zap: Switching from MAE to MAPE

--- CNN/lightning_modules.py
+++ CNN/lightning_modules.py
@@ -1,4 +1,5 @@
 import torch
+from torchmetrics.regression import MeanAbsolutePercentageError
 
 import lightning as L
 from model import BasicCNN
@@ -7,15 +8,15 @@ class CNNModule(L.LightningModule):
     def __init__(self):
         super().__init__()
         self.model = BasicCNN()
-        self.loss = torch.nn.L1Loss()
+        self.loss = MeanAbsolutePercentageError()
     def training_step(self, batch, batch_idx, mode="Train"):
         x, y_ref = batch
         y_hyp = self.model(x)
         loss = self.loss(y_hyp, y_ref)
-        self.log(f"{mode} MAE", loss)
+        self.log(f"{mode} MAPE (%)", loss * 100)
         return loss
     def validation_step(self, batch, batch_idx):
-        return self.training_step(batch, batch_idx, mode="Valid")
+        return self.training_step(batch, batch_idx, mode="Validation")
     def test_step(self, batch, batch_idx):
         return self.training_step(batch, batch_idx, mode="Test")
     def configure_optimizers(self):
